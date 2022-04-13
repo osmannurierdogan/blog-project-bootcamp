1b3c65cecc3c68feb5286d22a49cd83d147a2ac7
Osman Nuri Erdoğan <osmerd04@gmail.com>
2022-04-14 02:37:07 +0300

    mongodb-connection url recovered.

--- mongodb-connection.js
+++ mongodb-connection.js
@@ -1,14 +1,11 @@
 const mongoose = require('mongoose');
 
 const main = async () => {
-	//await mongoose.connect('mongodb://localhost:27017/bootcamp-blog', {
-	await mongoose.connect(
-		'mongodb://quiet-liger-2d1b66.netlify.app/bootcamp-blog',
-		{
-			useNewUrlParser: true,
-			useUnifiedTopology: true,
-		},
-	);
+	await mongoose.connect('mongodb://localhost:27017/bootcamp-blog', {
+	//await mongoose.connect('mongodb://quiet-liger-2d1b66.netlify.app/bootcamp-blog', {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	});
 	console.log("Connected to MongoDB");
 };
 main();
